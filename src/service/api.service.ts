91e9948b84ccbefb800ae89fbf8233fb29ed18fe
DanaXans <danaxans@gmail.com>
2026-04-18 21:38:49 +0300

    cant finish anyway task4

--- src/service/api.service.ts
+++ src/service/api.service.ts
@@ -1,10 +1,9 @@
 import type {CartResponseDummyJson} from "../models/CartResponseDummyjson.ts";
-import type {CartModel} from "../models/CartModel.ts";
+import type {CartI} from "../models/ProductModel.ts";
 const dataCarts = import.meta.env.VITE_API_BASE_URL + '/carts';
 
-export const loadCarts = async (): Promise<CartModel[]> => {
+export const getCarts = async (): Promise<CartI[]> => {
     const response: CartResponseDummyJson = await fetch(dataCarts)
         .then(value => value.json())
-
     return response.carts;
 }
