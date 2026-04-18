4fe0458c910e2a43062105c78987c7b9c0a7c26b
DanaXans <danaxans@gmail.com>
2026-04-18 21:56:29 +0300

    fin

--- src/components/product-component/Product.tsx
+++ src/components/product-component/Product.tsx
@@ -3,14 +3,15 @@ import type {ProductModel} from "../../models/ProductModel.ts";
 import './Product.css'
 
 type Prop = { product: ProductModel };
-export const Product: FC<Prop> = ({product: {title, brand, description, price}}) => {
+export const Product: FC<Prop> = ({product: {title, brand, description, price, thumbnail}}) => {
 
     return (
         <>
             <section className='m-7'>
-                <h3>Brand: {brand}</h3>
-                <h2>{title}</h2>
+                <h3>{brand}</h3>
+                <h2>{title}:</h2>
                 <div className='wrapper'>
+                    <img src={thumbnail} alt={title}/>
                     <p>{description}</p> <p>Price: {price}</p>
                 </div>
             </section>
