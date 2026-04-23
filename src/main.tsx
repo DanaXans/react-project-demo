a6f888aedaf4b801c1c4b549d796d6a37a3877da
DanaXans <danaxans@gmail.com>
2026-04-23 17:55:45 +0300

    fin task1 part2

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,20 @@
 import {createRoot} from "react-dom/client";
-import {RouterProvider} from "react-router";
-import {routes} from "./routes/Route.tsx";
+import {BrowserRouter, Routes} from "react-router";
+import {Route} from "react-router/internal/react-server-client";
+import {UsersComponent} from "./components/UsersComponent.tsx";
+import {PostsComponent} from "./components/PostsComponent.tsx";
+import {CommentsComponent} from "./components/CommentsComponent.tsx";
+import {ProductsComponent} from "./components/ProductsComponent.tsx";
+import {Layout} from "./Layout.tsx";
 
 createRoot(document.getElementById('root')!)
-
-    .render(<RouterProvider router={routes}></RouterProvider>)
+    .render(<BrowserRouter>
+        <Routes>
+            <Route path={'/'} element={<Layout/>}>
+                <Route path={'users'} element={<UsersComponent/>}/>
+                <Route path={'posts'} element={<PostsComponent/>}/>
+                <Route path={'comments'} element={<CommentsComponent/>}/>
+                <Route path={'products'} element={<ProductsComponent/>}/>
+            </Route>
+        </Routes>
+    </BrowserRouter>);
