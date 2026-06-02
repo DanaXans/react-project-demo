1b6d23b020b3e22fe5cff81a111f9892021ae98d
DanaXans <danaxans@gmail.com>
2026-06-02 13:10:53 +0300

    all done hw6

--- src/models/IBaseResponceModelType.ts
+++ src/models/IBaseResponceModelType.ts
@@ -1,9 +1,8 @@
 import type {IProduct} from "./IProduct.ts";
 
-export type IBaseResponceModelType = {//тип відповіді сервера при отриманні списку товарів
-    total: number;
-    skip: number;
-    limit: number;
-    products: IProduct[];
-
+export interface IBaseResponceModelType {//тип відповіді сервера при отриманні списку товарів
+    total: number
+    skip: number
+    limit: number
+    products: IProduct[]
 }
